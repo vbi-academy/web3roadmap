4359f092f91a5f95ef4ea78b2c99dca7afda9ef4
terrancrypt <thaiphamngoctuong@gmail.com>
2024-06-15 19:30:56 +0700

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [react()]
+  integrations: [react(), tailwind()]
 });
