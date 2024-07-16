6971392534935ada223cc290f73e2c6f398fe8f6
terrancrypt <thaiphamngoctuong@gmail.com>
2024-07-16 18:40:34 +0700

    move type to this file

--- src/lib/type.ts
+++ src/lib/type.ts
@@ -3,4 +3,26 @@ export interface MarkdownFileType<T = Record<string, string>> {
   file: string;
   url?: string;
   Content: any;
+  getHeadings: () => {
+    depth: number;
+    slug: string;
+    text: string;
+  }[];
+}
+
+export interface Subtopic {
+  subtopic_id: number;
+  subtopic_name: string;
+}
+
+export interface Topic {
+  topic_id: number;
+  topic_name: string;
+  subtopics?: Subtopic[];
+}
+
+export interface Phase {
+  phase_id: number;
+  phase_name: string;
+  topics: Topic[];
 }
