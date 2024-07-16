e14e8b3c535223ae48db738fe579afdc7e0f65d1
terrancrypt <thaiphamngoctuong@gmail.com>
2024-07-16 18:39:12 +0700

    using node for hybrid server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
-  integrations: [react(), tailwind()]
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone",
+  }),
+  integrations: [react(), tailwind()],
 });
