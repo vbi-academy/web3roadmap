ea603d7c6ae2e74ea04bf5703dedfb161656b55a
terrancrypt <thaiphamngoctuong@gmail.com>
2024-07-17 09:52:02 +0700

    add sitemap for SEO

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const SITE_DESCRIPTION = "Opensource Roadmap For Next-Gen Developers";
 export const SITE_TITLE = "Web3Roadmap";
+export const SITE_URL = "https://web3roadmap.com";
---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from "astro";
+import { SITE_URL } from "src/constants";
+
+const robotsTxt = `
+User-agent: *
+Allow: /
+
+Sitemap: ${new URL("sitemap-index.xml", SITE_URL).href}
+`.trim();
+
+export const GET: APIRoute = () => {
+  return new Response(robotsTxt, {
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8",
+    },
+  });
+};
